03ad004451856b9b63e45dcead87033630ef55ee
Andrey Nering <andrey.nering@gmail.com>
2015-12-21 15:14:40 -0200

    Initial commit. Minimal HTML boilerplate with webpack.

---
+++ bundle.js
@@ -0,0 +1,1 @@
+!function(r){function e(o){if(t[o])return t[o].exports;var n=t[o]={exports:{},id:o,loaded:!1};return r[o].call(n.exports,n,n.exports,e),n.loaded=!0,n.exports}var t={};return e.m=r,e.c=t,e.p="",e(0)}([function(r,e,t){alert(t(1))},function(r,e){r.exports="Hello, webpack"}]);
